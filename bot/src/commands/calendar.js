e6dd0bb480993fa4a11e510b0301e848f932e728
Garry Trinder <garry@trinder365.co.uk>
2022-12-08 15:23:00 +0000

    Adds Calendar card

--- bot/src/commands/calendar.js
+++ bot/src/commands/calendar.js
@@ -1,11 +1,16 @@
-const { MessageFactory } = require("botbuilder")
+const { MessageFactory, CardFactory } = require("botbuilder")
+const data = require('../data.json');
+const { AdaptiveCards } = require("@microsoft/adaptivecards-tools");
+const calendarCard = require('../adaptiveCards/calendar.json');
 
 class CalendarCommandHandler {
 
   triggerPatterns = 'calendar'
 
   async handleCommandReceived(context, message) {
-    return MessageFactory.text('Calendar command received');
+    const cardData = { ...data, siteendpoint: process.env.SITE_ENDPOINT };
+    const cardJson = AdaptiveCards.declare(calendarCard).render(cardData);
+    return MessageFactory.attachment(CardFactory.adaptiveCard(cardJson));
   }
 
 }
